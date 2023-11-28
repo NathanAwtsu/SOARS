5632d5c9a0f9794b6793071db74e3ce8122e15d6
Nathan <tan.com.nlm@gmail.com>
2023-11-29 06:33:08 +0800

    Minor Fix

--- resources/views/navbar/admin_nav.blade.php
+++ resources/views/navbar/admin_nav.blade.php
@@ -21,7 +21,7 @@
     <script src="{{url('https://code.jquery.com/jquery-3.6.0.min.js')}}"></script>
     <script src="{{url('https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js')}}"></script>
     <script src="{{url('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js')}}"></script>
-    <script src="{{ url('https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{url('https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js')}}"></script>
     
     @stack('jquery')
     @stack('styles')
